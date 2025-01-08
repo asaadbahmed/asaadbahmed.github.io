c3691b9a74513925da9b2a5d1f6fff5e3bded317
Asaad Ahmed <asaadbinahmed@gmail.com>
2025-01-08 17:48:40 -0500

    color changes, added theme switch, switched borderradius to use em instead of px

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,8 @@ import {
   FaRegFilePdf,
 } from "react-icons/fa";
 
+import { styled } from "@mui/material/styles";
+import Switch from "@mui/material/Switch";
 import { useEffect, useState } from "react";
 import "./App.css";
 
@@ -58,9 +60,9 @@ function Spotlight({ children, opacity }) {
         minWidth: "70%",
         minHeight: "7%",
 
-        borderRadius: "24px",
+        borderRadius: "2em",
         border: "2px solid rgb(40, 40, 40)",
-        backgroundColor: "rgb(20, 20, 20)", // IMPORTANT: rgb(40, 40, 40) looks nice too, get opinions
+        backgroundColor: "rgb(30, 30, 30)", // IMPORTANT: 20 & 40 look nice too, get opinions
         boxShadow: "0 0 10px 2px rgba(40, 40, 40, 0.6)",
 
         display: "flex",
@@ -77,6 +79,66 @@ function Spotlight({ children, opacity }) {
   );
 }
 
+const IOSSwitch = styled((props) => (
+  <Switch focusVisibleClassName=".Mui-focusVisible" disableRipple {...props} />
+))(({ theme }) => ({
+  width: 42,
+  height: 26,
+  padding: 0,
+  "& .MuiSwitch-switchBase": {
+    padding: 0,
+    margin: 2,
+    transitionDuration: "300ms",
+    "&.Mui-checked": {
+      transform: "translateX(16px)",
+      color: "#fff",
+      "& + .MuiSwitch-track": {
+        backgroundColor: "#65C466",
+        opacity: 1,
+        border: 0,
+        ...theme.applyStyles("dark", {
+          backgroundColor: "#2ECA45",
+        }),
+      },
+      "&.Mui-disabled + .MuiSwitch-track": {
+        opacity: 0.5,
+      },
+    },
+    "&.Mui-focusVisible .MuiSwitch-thumb": {
+      color: "#33cf4d",
+      border: "6px solid #fff",
+    },
+    "&.Mui-disabled .MuiSwitch-thumb": {
+      color: theme.palette.grey[100],
+      ...theme.applyStyles("dark", {
+        color: theme.palette.grey[600],
+      }),
+    },
+    "&.Mui-disabled + .MuiSwitch-track": {
+      opacity: 0.7,
+      ...theme.applyStyles("dark", {
+        opacity: 0.3,
+      }),
+    },
+  },
+  "& .MuiSwitch-thumb": {
+    boxSizing: "border-box",
+    width: 22,
+    height: 22,
+  },
+  "& .MuiSwitch-track": {
+    borderRadius: 26 / 2,
+    backgroundColor: "#E9E9EA",
+    opacity: 1,
+    transition: theme.transitions.create(["background-color"], {
+      duration: 500,
+    }),
+    ...theme.applyStyles("dark", {
+      backgroundColor: "#39393D",
+    }),
+  },
+}));
+
 export default function App() {
   const [spotlightOpacity, setOpacity] = useState(100);
   const [theme, setTheme] = useState("dark");
@@ -90,7 +152,7 @@ export default function App() {
     );
   const resumeOnClick = () => window.open(Resume);
   const mailOnClick = () => window.open("mailto:asaadbinahmed@gmail.com");
-  
+
   return (
     <div
       style={{ maxHeight: "100vh", display: "flex", flexDirection: "column" }}
@@ -114,10 +176,10 @@ export default function App() {
             display: "flex",
             flexDirection: "row",
             justifyContent: "center",
-            gap: "10px", //8px, 12%
+            gap: "10px",
             alignSelf: "center",
             marginLeft: "auto",
-            marginRight: "4%", // 20px
+            marginRight: "4%",
             overflow: "hidden",
           }}
         >
@@ -158,6 +220,11 @@ export default function App() {
       >
         <Heading />
       </FadeInSection>
+      <div style={{ position: "fixed", right: "4%", bottom: "4%" }}>
+        <IOSSwitch checked={theme == "dark" ? true : false} onChange={event => {          
+          event.target.checked ? setTheme("dark") : setTheme("light");
+        }} />
+      </div>
     </div>
   );
 }
