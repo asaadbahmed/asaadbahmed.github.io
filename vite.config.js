2ca59cdf5641d3f11831c918f097fa63f3fd7ffc
Asaad Ahmed <asaadbinahmed@gmail.com>
2025-01-25 02:48:37 -0500

    wiped project clean

--- vite.config.js
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
-import svgr from "vite-plugin-svgr";
-
-// https://vite.dev/config/
-export default defineConfig({
-  base: "./asaadbahmed.github.io",
-  plugins: [react(), svgr()],
-});
