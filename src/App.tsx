143b4bbcf45ad59053926044ab7c9fc493e17c9e
Asaad Ahmed <asaadbinahmed@gmail.com>
2025-10-03 02:44:56 -0400

    updated about me

--- src/App.tsx
+++ src/App.tsx
@@ -3,20 +3,16 @@ import { Navigation } from "@/components/navigation";
 
 function App() {
   return (
-    <div className="flex flex-col gap-4">
-      <Navigation />
-      <div className="flex flex-col items-center gap-2">
-        <h1 className="scroll-m-20 text-center text-3xl font-bold tracking-normal text-balance">
-          Coming Soon
-        </h1>
-        <h3 className="scroll-m-20 text-2xl font-semibold tracking-normal text-left">
-          hi, I'm <span className="text-emerald-400">Asaad!</span> <br />• i'm
-          studying CE at tmu (formerly ry)
-          <br />• i build stuff in my freetime
-          <br />• i run a clothing business
-          <br />• and i enjoy learning arabic
-        </h3>
+    <div className="flex flex-col items-center">
+      <div className="bg-[rgb(247,247,249)] w-md rounded-3xl p-4">
+        <div className="scroll-m-20 text-sm text-muted-foreground font-medium tracking-normal text-left space-y-2">
+          <div>i'm studying CE at tmu (formerly ry)</div>
+          <div>i build stuff in my freetime</div>
+          <div>i run a clothing business</div>
+          <div>and i enjoy learning arabic</div>
+        </div>
       </div>
+      <Navigation />
     </div>
   );
 }
