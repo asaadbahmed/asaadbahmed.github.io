6291ec0f9fb36cb466af66e3f52df670058d993b
Asaad Ahmed <asaadbinahmed@gmail.com>
2025-10-03 03:34:27 -0400

    updated layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,41 @@
 import "./App.css";
+import { Separator } from "@/components/ui/separator";
 import { SocialsDock } from "@/components/socials-dock";
+
+const AboutCard = () => (
+  <div className="scroll-m-20 text-sm text-muted-foreground font-medium tracking-normal text-left space-y-2">
+    <div>i'm studying CE at tmu (formerly ry)</div>
+    <div>i build stuff in my freetime</div>
+    <div>i run a clothing business</div>
+    <div>i enjoy learning arabic</div>
+  </div>
+);
+
+const SummaryCard = () => (
+  <div className="w-full max-w-sm aspect-square bg-[rgb(247,247,249)] rounded-3xl p-4 flex flex-col gap-4">
+    <div className="text-left text-2xl font-semibold">
+      Asaad — B.E. in CE{" "}
+      <span className="font-semibold text-muted-foreground">('28)</span>
+    </div>
+    <Separator />
+    <div className="flex flex-col text-left justify-between text-sm text-muted-foreground font-medium space-y-4">
+      <div>
+        i think software engineering is, at its core, problems and solutions.
+      </div>
+      <div>sometimes the harder part is identifying the right problem.</div>
+      <div>and there’s rarely only one solution.</div>
+      <div>so we should keep our minds open and experiment.</div>
+      <div>with curiosity and care, we can build things that matter.</div>
+      <div>things that make the world a better place.</div>
+    </div>
+  </div>
+);
 
 function App() {
   return (
-    <div className="flex flex-col items-center">
-      <div className="bg-[rgb(247,247,249)] w-md rounded-3xl p-4">
-        <div className="scroll-m-20 text-sm text-muted-foreground font-medium tracking-normal text-left space-y-2">
-          <div>i'm studying CE at tmu (formerly ry)</div>
-          <div>i build stuff in my freetime</div>
-          <div>i run a clothing business</div>
-          <div>and i enjoy learning arabic</div>
-        </div>
+    <div className="flex flex-col items-center pl-20 pr-20">
+      <div className="flex flex-row flex-1">
+        <SummaryCard />
       </div>
       <SocialsDock />
     </div>
