a26d9e4216d75696c4018d270e2fbc28cb3a8da4
Asaad Ahmed <asaadbinahmed@gmail.com>
2025-10-08 03:32:44 -0400

    made a bento layout for the website

--- src/App.tsx
+++ src/App.tsx
@@ -12,7 +12,7 @@ import { Navbar } from "@/components/navigation-bar";
 // );
 
 const SummaryCard = () => (
-  <div className="bg-[rgb(247,247,249)] rounded-3xl p-4 flex flex-col gap-4">
+  <div className="bg-[rgb(247,247,249)] rounded-3xl p-4 flex flex-col gap-8">
     <div className="text-left text-xl font-semibold">
       Asaad — B.E. in Computer Engineering{" "}
       <span className="font-semibold text-muted-foreground">('28)</span>
@@ -32,13 +32,43 @@ const SummaryCard = () => (
 
 function App() {
   return (
-    <div className="flex flex-col items-center">
-      <div className="pb-10">
+    <div>
+      <div className="flex flex-col items-center mb-10">
         <Navbar />
       </div>
-      <div className="flex flex-col gap-10 mb-20">
-        <div className="text-4xl font-semibold">Coming Soon</div>
-        <SummaryCard />
+      <div className="max-w-6xl mx-auto">
+        <div className="grid grid-cols-4 grid-rows-2 gap-4">
+          <div className="bg-blue-500 col-span-2 row-span-2 aspect-square rounded-3xl">
+            Hero
+          </div>
+
+          <div className="bg-green-500 col-span-2 row-span-1 rounded-3xl">
+            Current Work (anytimeoutfits.com)
+          </div>
+
+          <div className="bg-teal-500 col-span-1 row-span-1 aspect-square rounded-3xl">
+            Barcode Generator
+          </div>
+          <div className="bg-purple-500 col-span-1 row-span-1 aspect-square rounded-3xl">
+            Email Sorter
+          </div>
+
+          <div className="bg-pink-500 col-span-1 row-span-2 aspect-[9/18] rounded-3xl">
+            Inventory Manager
+          </div>
+          <div className="bg-rose-500 col-span-1 row-span-2 aspect-[9/18] rounded-3xl">
+            Robot
+          </div>
+          <div className="bg-amber-500 col-span-1 row-span-2 aspect-[9/18] rounded-3xl">
+            TBD 1
+          </div>
+          <div className="bg-orange-500 col-span-1 row-span-2 aspect-[9/18] rounded-3xl">
+            TBD 2
+          </div>
+          <div className="bg-sky-500 col-span-4 row-span-1 aspect-[16/2] rounded-3xl">
+            Tech Stack
+          </div>
+        </div>
       </div>
     </div>
   );
